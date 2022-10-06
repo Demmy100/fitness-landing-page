4f15976ede40e063a17a59f4e68d2ab4ec1d9d36
akinolademilade <demiladeaduragbemi@gmail.com>
2022-10-06 20:34:24 +0100

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,24 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.React_App_Rapid_Api_key,
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    },
+  };
+
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.React_App_Rapid_Api_key_youtube,
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+  };
+  
+
+
+export const fetchData = async (url, options) => {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    return data;
+}
